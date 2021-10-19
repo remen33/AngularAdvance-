5099686a9f64f4ed821917c169ca51ec64cd5f8f
Emerson Naranjo <emer.tenor.gmail.com>
2021-10-19 11:44:18 -0500

    step up register and no page found

--- 01-adminpro/src/app/pages/nopagefound/nopagefound.component.ts
+++ 01-adminpro/src/app/pages/nopagefound/nopagefound.component.ts
@@ -3,14 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./nopagefound.component.css'],
 })
 export class NopagefoundComponent implements OnInit {
+  year = new Date().getFullYear();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
